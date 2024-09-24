f7ada0eba379c9c6284ee06bfb4dd89cad858c02
Pasquale Salza <pasquale.salza@gmail.com>
2024-09-24 15:17:11 +0200

    Added the resolve field to python_distribution.

--- pants-plugins/multiresolve/register.py
+++ pants-plugins/multiresolve/register.py
@@ -1,10 +1,5 @@
-from multiresolve import target_types_rules
-from multiresolve.target_types import MultiResolvePexBinaryTarget
-
-
-def target_types():
-    return (MultiResolvePexBinaryTarget,)
-
+from pants.backend.python.target_types import PythonDistribution, PythonResolveField
 
 def rules():
-    return (*target_types_rules.rules(),)
+    # return (*target_types_rules.rules(),)
+    return [PythonDistribution.register_plugin_field(PythonResolveField)]
