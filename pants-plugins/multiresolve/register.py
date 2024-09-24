20c209cd08a210516094ddabdac4a56f36464059
Pasquale Salza <pasquale.salza@gmail.com>
2024-09-24 15:53:00 +0200

    Add resolve field to python_distribution.

--- pants-plugins/multiresolve/register.py
+++ pants-plugins/multiresolve/register.py
@@ -1,5 +1,6 @@
 from pants.backend.python.target_types import PythonDistribution, PythonResolveField
 
+
 def rules():
     # return (*target_types_rules.rules(),)
     return [PythonDistribution.register_plugin_field(PythonResolveField)]
