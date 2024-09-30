fef91566df943dbdd59582367110ca422d51461c
Pasquale Salza <pasquale.salza@gmail.com>
2024-09-30 12:32:16 +0200

    Refactor plugin registration and resource handling
    This commit refactors the plugin registration and resource handling in the `pants-plugins/multiresolve/register.py` file.
    
    - Added `ResourcesGeneratorTarget` and `ResourceTarget` to the list of registered plugin fields.
    - Moved the `PythonResolveField` to the `ResourcesGeneratorTarget.moved_fields` list.
    - Updated the `rules()` function to include the new plugin fields.
    
    The changes in this commit improve the organization and extensibility of the codebase.

--- pants-plugins/multiresolve/register.py
+++ pants-plugins/multiresolve/register.py
@@ -1,6 +1,17 @@
 from pants.backend.python.target_types import PythonDistribution, PythonResolveField
+from pants.core.target_types import ResourcesGeneratorTarget, ResourceTarget
 
 
 def rules():
-    # return (*target_types_rules.rules(),)
-    return [PythonDistribution.register_plugin_field(PythonResolveField)]
+    # Hack the moved fields of the ResourcesGeneratorTarget to include PythonResolveField when generating a ResourceTarget.
+    ResourcesGeneratorTarget.moved_fields = (
+        *ResourcesGeneratorTarget.moved_fields,
+        PythonResolveField,
+    )
+
+    # Adding the PythonResolveField to core targets.
+    return [
+        PythonDistribution.register_plugin_field(PythonResolveField),
+        ResourcesGeneratorTarget.register_plugin_field(PythonResolveField),
+        ResourceTarget.register_plugin_field(PythonResolveField),
+    ]
